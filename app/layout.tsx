5ae039c00e96770d90692f274baccfe4b3dc1127
Bhabishya Luitel <bhabishyaluitel@gmail.com>
2024-02-20 21:56:26 +0545

    First page of my portfolio website is configured.

---
+++ app/layout.tsx
@@ -0,0 +1,60 @@
+import "../globals.css";
+import React from 'react';
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Bhabishya Luitel",
+    template: "Bhabishya Luitel",
+  },
+  description: "Full-Stack Web Developer",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Bhabishya Luitel",
+    card: "summary_large_image",
+  },
+  icons: {
+    shortcut: "/favicon.png",
+  },
+};
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <head>
+        {/* <Analytics /> */}
+      </head>
+      <body
+        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+          }`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
