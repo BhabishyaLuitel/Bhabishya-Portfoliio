fc57ec7c6a0dffc2e8984fcdb0004fa5699fc65b
Bhabishya Luitel <bhabishyaluitel@gmail.com>
2024-02-23 20:08:41 +0545

    Added Projects Page

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -35,6 +35,18 @@ export const Navigation: React.FC = () => {
 							Projects
 						</Link>
 						<Link
+							href="/about"
+							className="duration-200 text-zinc-400 hover:text-zinc-100"
+						>
+							About
+						</Link>
+						<Link
+							href="/learn"
+							className="duration-200 text-zinc-400 hover:text-zinc-100"
+						>
+							Learn
+						</Link>
+						<Link
 							href="/contact"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -31,7 +31,7 @@ export default function Example() {
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Particles
         		className="absolute inset-0 -z-10"
-        		quantity={1000}
+        		quantity={500}
      		 />
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
